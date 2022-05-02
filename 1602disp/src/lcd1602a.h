cb49b2f6304fc6f07a8b473dedcdba476f90571d
huxiaoan <hu7006@outlook.com>
2022-05-02 23:19:45 +0800

    控制信号宏定义函数

---
+++ 1602disp/src/lcd1602a.h
@@ -0,0 +1,50 @@
+#ifndef _H_LCD1602A_
+#define _H_LCD1602A_
+
+#define CTR_PORT P6
+#define RS_BIT 2
+#define RW_BIT 1
+#define EN_BIT 0
+
+#define DATA_PORT P5
+
+#define RS(x)                        \
+    do                               \
+    {                                \
+        if (x)                       \
+        {                            \
+            P6 |= 0x01 << RS_BIT;    \
+        }                            \
+        else                         \
+        {                            \
+            P6 &= ~(0x01 << RS_BIT); \
+        }                            \
+    } while (0)
+
+#define RW(x)                        \
+    do                               \
+    {                                \
+        if (x)                       \
+        {                            \
+            P6 |= 0x01 << RW_BIT;    \
+        }                            \
+        else                         \
+        {                            \
+            P6 &= ~(0x01 << RW_BIT); \
+        }                            \
+    } while (0)
+
+#define EN(x)                        \
+    do                               \
+    {                                \
+        if (x)                       \
+        {                            \
+            P6 |= 0x01 << EN_BIT;    \
+        }                            \
+        else                         \
+        {                            \
+            P6 &= ~(0x01 << EN_BIT); \
+        }                            \
+    } while (0)
+
+#endif
