17d0ab193eede34b3d7cd9f06967c4fa1633ded7
huxiaoan <hu7006@outlook.com>
2022-05-02 23:41:17 +0800

    修改等待函数

--- 1602disp/src/LCD1602A.c
+++ 1602disp/src/LCD1602A.c
@@ -4,7 +4,8 @@
 #include "lcd1602a.h"
 #include "type_def.h"
 
-void WriteInsWithoutFlagCheck(uin8_t ins){
+void WriteInsWithoutFlagCheck(uin8_t ins)
+{
     RS(0);
     RW(0);
     EN(1);
@@ -12,20 +13,19 @@ void WriteInsWithoutFlagCheck(uin8_t ins){
     EN(0);
 }
 
-void WriteIns(uint8_t ins){
-    while(isBusy());
-    EN(0);
+void WriteIns(uint8_t ins)
+{
+    waitWhileBusy();
     WriteInsWithoutFlagCheck(ins);
 }
 
-
-uint8_t isBusy(void){
+uint8_t waitWhileBusy(void)
+{
     //检查数据口最高位
-    DATA_PORT=0xff;
+    DATA_PORT = 0xff;
     RS(0);
     RW(1);
     EN(1);
-
-    return DATA_PORT&0x80;
-    
+    while (DATA_PORT & 0x80);
+    EN(0);
 }
