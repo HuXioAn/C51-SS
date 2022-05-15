f0ebaa826f7ca7c0db7953fd23d1810bbc7eb847
胡小安 <hu7006@outlook.com>
2022-05-15 15:29:43 +0800

    显示响应按键动作基本完成

--- 1602disp/src/LCD1602A.c
+++ 1602disp/src/LCD1602A.c
@@ -4,6 +4,17 @@
 #include "lcd1602a.h"
 #include "type_def.h"
 #include "delay.h"
+#include <string.h>
+
+typedef struct diaplay_s{
+    //这里的指针都是加一同侧计数方式
+    char * buffer;
+    uint8_t buffer_len;
+    char * lcd_head;
+    char * cursor;//下一个字显示的位置
+    char * lcd_tail;//显示区域末尾字符的下一个
+    char * str_end;//已经填充内容的末尾字符加一
+}display_t;
 
 void WriteInsWithoutFlagCheck(uint8_t ins)
 {
@@ -84,6 +95,117 @@ uint8_t lcd1602_printChar(char * str,uint8_t line,int8_t pos){
         str++;
     }
 }
+
+void lcd1602_dispStructInit(display_t * disp,char * buffer,uint8_t buffer_len){
+    //初始化显示结构体
+    disp->buffer=buffer;
+    disp->buffer_len=buffer_len;
+    disp->lcd_head=buffer;
+    disp->cursor=buffer;
+    disp->lcd_tail=buffer+LCD1602_WIDTH;
+    disp->str_end=buffer;
+    //*(buffer+buffer_len-1)=0;
+}
+
+
+uint8_t lcd1602_dispKeyValue(char keyvalue, display_t * disp ){
+    //根据键值命令显示
+    switch (keyvalue)
+    {//先判断控制字符，显示字符统一处理
+    case 'l':
+        if(disp->cursor == disp->buffer){//在算式最左边，跳转最右边
+            disp->cursor = disp->str_end;
+            if(disp->cursor >= disp->lcd_head && disp->cursor < disp->lcd_tail){
+                break;
+            }
+            disp->lcd_tail= disp->cursor+1;
+            disp->lcd_head=disp->lcd_tail-LCD1602_WIDTH;
+            break;
+        }
+        disp->cursor--;
+        if(disp->cursor == (disp->lcd_head-1)){//如果到最左了，显示区域位移
+            disp->lcd_head--;
+            disp->lcd_tail--;
+        }
+
+        break;
+    case 'r':
+        if(disp->cursor == disp->str_end){//在算式最右边，跳转最左边
+            disp->cursor = disp->buffer;
+            if(disp->cursor >= disp->lcd_head && disp->cursor < disp->lcd_tail){
+                break;
+            }
+            disp->lcd_head=disp->buffer;
+            disp->lcd_tail=disp->buffer+LCD1602_WIDTH;
+            break;
+        }
+        disp->cursor++;
+        if(disp->cursor == (disp->lcd_tail)){//如果到最左了，显示区域位移
+            disp->lcd_head++;
+            disp->lcd_tail++;
+        }
+        break;
+    case '=':
+        return 1;//返回通知主程序开始计算
+        break;
+    case '\e'://清零
+        lcd1602_dispStructInit(disp,disp->buffer,disp->buffer_len);
+        lcd1602_WriteIns(0x01);
+        delay_us(1700);
+        break;
+    case '\b':
+        if(disp->cursor == disp->buffer)break;//如果已空，就无任何反应
+        if(disp->cursor == disp->str_end){//在最后面删除，没有影响
+            disp->cursor--;
+            disp->str_end--;
+        }else{//在内容中删除
+            strncpy(disp->cursor-1,disp->cursor,disp->str_end-disp->cursor);
+            disp->cursor--;
+            disp->str_end--;
+        }
+        if(disp->cursor == (disp->lcd_head-1)){//如果到最左了，显示区域位移
+            disp->lcd_head--;
+            disp->lcd_tail--;
+        }
+
+        break;
+    default:
+        //要显示的字符
+        if(disp->cursor == (disp->buffer+disp->buffer_len-2))break;//如果已满，就无任何反应
+        *(disp->cursor++)=keyvalue;
+        disp->str_end++;
+        if(disp->cursor == disp->lcd_tail){//如果到右边了，显示区域位移
+            disp->lcd_head++;
+            disp->lcd_tail++;
+        }
+        break;
+    }
+
+    //更新显示
+
+
+    return 0;
+}
+
+
+void lcd1602_displayFromStruct(display_t * disp){
+    char content[LCD1602_WIDTH+1];
+    
+    *(disp->str_end)='\0';
+    strncpy(content,disp->lcd_head,LCD1602_WIDTH);
+    content[LCD1602_WIDTH]='\0';
+
+    lcd1602_printChar(content,1,0);//发送内容
+
+    lcd1602_WriteIns(0x80|(disp->cursor-disp->lcd_head));//设定光标
+	delay_us(40); 
+
+
+
+}
+
+
+
 
 
 
